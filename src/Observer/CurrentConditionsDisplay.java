98d913c6c6c62db06c6e3d5e63b7a102b5d3c49d
MiddleTime <nadoroge@gmail.com>
2019-01-11 14:17:37 +0200

    Testing2

---
+++ src/Observer/CurrentConditionsDisplay.java
@@ -0,0 +1,28 @@
+package Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+
+    }
+
+
+    @Override
+    public void display() {
+        System.out.println("current conditions: " + temperature + "C degree and " + humidity + "% humidity" + "pressure " + pressure);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+}
--- src/Observer/Main.java
+++ src/Observer/Main.java
@@ -10,5 +10,19 @@ package Observer;
  * @author inbox_bv8awe9
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay cuurrentDisplay = new CurrentConditionsDisplay(weatherData);
+        
+
+        weatherData.setMeasurements(30, 65, 30.2f);
+        weatherData.setMeasurements(20, 75, 29.2f);
+        weatherData.setMeasurements(31, 95, 29.3f);
+    }
 }
+
+
+// StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        //  ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
