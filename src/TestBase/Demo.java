f4c613629c4fba4b038307be8eeafb3e7ea7ee35
MiddleTime <nadoroge@gmail.com>
2019-03-15 15:03:10 +0200

    test Maven Database connection

--- src/TestBase/Demo.java
+++ src/TestBase/Demo.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Demo {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift="
+        String url = "jdbc:mysql://localhost:3306/mysql?useUnicode=true&useJDBCCompliantTimezoneShift="
                 + "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         String username = "root";
         String password = "";
@@ -15,10 +15,10 @@ public class Demo {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(2)); //выведем данные по второй колонке
+                System.out.println(resultSet.getString("Host") + " " + resultSet.getString("User")); //выведем данные по второй колонке
             }
             resultSet.close();
             statement.close();
